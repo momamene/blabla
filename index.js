e44aa65b8f017bcf11855799cbee86923e6b87d7
momamene <momamene@gmail.com>
2014-01-11 17:18:19 +0900

    Item increased every 10 seconds.

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 var RSS = require('rss');
+var request = require('request');
+var http = require("http");
 
 /* lets create an rss feed */
 var feed = new RSS({
     title: 'BlaBla Magazine',
     description: 'Publish world the most worthless news',
-    feed_url: 'http://example.com/rss.xml',
-    site_url: 'http://blabla.com',
-    image_url: 'http://example.com/icon.png',
-    docs: 'http://example.com/rss/docs.html',
     author: 'Hyuntak Joo',
-    managingEditor: 'Hyuntak Joo',
-    webMaster: 'Hyuntak Joo',
     copyright: 'Copyright (c) 2014 BIGMAMA All rights reserved',
-    language: 'ko',
     pubDate: 'January 11, 2014 04:00:00 GMT'
 });
+var feedxml = feed.xml().replace(/pubDate/gi, 'dc:date').replace(/dc:creator/gi, 'author');
+setInterval(function() {
+    request('http://hangul.thefron.me/generate.json?&paragraphs=2&length=short', function (error, response, body) {
+        var paragraphs = JSON.parse(body).ipsum.split('<br><br>'),
+            title = paragraphs[0].split('.')[0],
+            author = paragraphs[0].split('.')[1].split(' ')[1];
+        feed.item({
+            title: title,
+            description: paragraphs[1],
+            author: author,
+            url: 'http://example.com/article4?this&that',
+            date: new Date()
+        });
+        feedxml = feed.xml().replace(/pubDate/gi, 'dc:date').replace(/dc:creator/gi, 'author');
+    });
+}, 10000);
 
-/* loop over data and add to feed */
-feed.item({
-    title:  'item title',
-    description: 'use this for the content. It can include html.',
-    url: 'http://example.com/article4?this&that', // link to the item
-    author: 'Guest Author', // optional - defaults to feed author property
-    date: 'January 11, 2014' // any format that js Date can parse.
-});
-
-// cache the xml to send to clients
-var xml = feed.xml().replace(/pubDate/gi, 'dc:date');
-console.log(xml);
+console.log('BlaBla server started');
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/xml"});
+  response.write(feedxml);
+  response.end();
+}).listen(8888);
